c6c97fdc88cda3203b356d4a06aaed4945b83cfc
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-11-15 16:45:57 +0600

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,29 @@ export default {
   theme: {
     extend: {
       colors: {
+        primary: "#7F56D9",
+        "primary-light": "#F7F3FF",
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      container: {
+        screens: {
+          DEFAULT: "1290px",
+        },
+        center: true,
+        padding: "1.2rem",
+      },
+      backgroundImage: {
+        "black-gradient":
+          "linear-gradient(178deg, rgba(0, 0, 0, 0.00) 44.14%, #000 98.09%)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      screens: {
+        xs: "540px", // min-width
       },
     },
   },
